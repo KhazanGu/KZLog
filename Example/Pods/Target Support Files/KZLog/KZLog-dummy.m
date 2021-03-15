c3bccd87251ed67b9e43e82d6d3bf60e1e2f6324
Khazan <Khazan.Gu@icloud.com>
2021-03-15 10:22:05 +0800

    first commit

---
+++ Example/Pods/Target Support Files/KZLog/KZLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KZLog : NSObject
+@end
+@implementation PodsDummy_KZLog
+@end
---
+++ Example/Pods/Target Support Files/Pods-KZLog_Tests/Pods-KZLog_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KZLog_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_KZLog_Tests
+@end
